456f89248d372c9caf5f4a35dd05842279a2ea7e
Heo Namjun <heonamjun@Heos-MacBook-Air.local>
2022-09-26 01:06:43 +0900

    [Feat/execute] ft_strcmp 추가

---
+++ srcs/execute/ft_strcmp.c
@@ -0,0 +1,13 @@
+#include "libft.h"
+
+int	ft_strcmp(const *str1, const *str2)
+{
+	while (*str1 || *str2)
+	{
+		if (*str1 != *str2)
+			return (*str1 - *str2);
+		str1++;
+		str2++;
+	}
+	return (*str1 - *str2);
+}
