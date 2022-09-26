8d112810e2527cef01c37a320c1a340ed191c342
tamagoyakii <rlawlgus4063@naver.com>
2022-09-26 14:40:03 +0900

    [Fix] 구조체 변경에 따른 수정 및 구조체 util 함수 적용 #12

--- srcs/parse/create_argvs.c
+++ srcs/parse/create_argvs.c
@@ -1,6 +1,6 @@
 # include "../includes/minishell.h"
 
-int	put_cmd(t_cmd *cmd, char **arr)
+static int	put_cmd(t_cmd *cmd, char **arr)
 {
 	t_list	*tmp;
 	int		i;
@@ -15,77 +15,79 @@ int	put_cmd(t_cmd *cmd, char **arr)
 		arr[i] = tmp->content;
 		tmp = tmp->next;
 	}
-	while (cmd->cmds)
-	{
-		tmp = cmd->cmds;
-		cmd->cmds = cmd->cmds->next;
-		free(tmp);
-	}
+	arr[i] = 0;
+	free_lst(cmd->cmds);
 	cmd->cnt = 0;
 	return (SUCCESS);
 }
 
-int	add_redir(t_argv *argvs, char *value, int r_type, int *last_type)
+static int	add_redir(t_argv *argvs, char *value, int r_type, int *last_type)
 {
-	t_type	*tmp;
+	t_type	*head;
+	t_type	*new;
 
-	if (*last_type == T_OUT || *last_type == A_OUT
-		|| *last_type == IN || *last_type == HDOC)
+	if (*last_type != PIPE && *last_type != WORD && *last_type != NONE)
 		return (FAIL);
 	if (r_type == T_OUT || A_OUT)
-	{
-		argvs->out = add_type(argvs->out, r_type, 0);
-		if (!argvs->out)
-			return (FAIL);
-	}
-	if (r_type == IN || HDOC)
-	{
-		argvs->in = add_type(argvs->in, r_type, 0);
-		if (!argvs->in)
-			return (FAIL);
-	}
+		head = argvs->out;
+	else
+		head = argvs->in;
+	new = create_type(r_type, 0);
+	if (!new)
+		return (FAIL);
+	ft_typeadd_back(&head, new);
 	if (r_type == HDOC)
-	{
-		argvs->hdoc = add_type(argvs->hdoc, r_type, 0);
-		if (!argvs->hdoc)
-			return (FAIL);
-	}
+		ft_typeadd_back(&argvs->hdoc, new);
+	free(value);
 	*last_type = r_type;
 	return (SUCCESS);
 }
 
-int	add_pipe(t_argv *argvs, t_cmd *cmd, int *last_type)
+static int	add_pipe(t_argv *argvs, t_cmd *cmd, int *last_type)
 {
-	if (*last_type == NONE)
+	t_argv	*new_argv;
+
+	if (*last_type != WORD)
 		return (FAIL);
 	if (!put_cmd(cmd, argvs->cmd))
 		return (FAIL);
 	*last_type = PIPE;
-	argvs = add_argv(argvs);
-	if (!argvs)
+	new_argv = create_argv();
+	if (!new_argv)
 		return (FAIL);
+	ft_argvadd_back(argvs, new_argv);
 	return (SUCCESS);
 }
 
-int	add_word(t_argv *argvs, t_list *cmds, char *value, int *last_type)
+static int	add_word(t_argv *argvs, t_cmd *cmd, char *value, int *last_type)
 {
+	t_list	*new_cmd;
+
 	if (*last_type == T_OUT || *last_type == A_OUT)
-		argvs->out->value = value;
-	else if (*last_type == IN || *last_type == HDOC)
-		argvs->in->value = value;
+		ft_typelast(argvs->out)->value = value;
+	else if (*last_type == IN)
+		ft_typelast(argvs->in)->value = value;
+	else if (*last_type == HDOC)
+	{
+		ft_typelast(argvs->in)->value = ft_strjoin("/tmp/", value);
+		ft_typelast(argvs->hdoc)->value = ft_strjoin("/tmp/", value);
+		free(value);
+	}
 	else
 	{
-		if (!ft_lstnew(value))
+		new_cmd = ft_lstnew(value);
+		if (!new_cmd)
 			return (FAIL);
+		ft_lstadd_back(cmd->cmds, new_cmd);
+		cmd->cnt += 1;
 	}
-	if (*last_type == HDOC)
-		argvs->hdoc->value = value;
 	*last_type = WORD;
 	return (SUCCESS);
 }
 
-int	create_argv(t_argv *argvs, t_type *tokens)
+int	create_argvs(t_argv *argvs, t_list *tokens)
 {
+	t_token	*token;
 	t_cmd	*cmd;
 	int		last_type;
 	int		r_type;
@@ -95,13 +97,14 @@ int	create_argv(t_argv *argvs, t_type *tokens)
 	last_type = NONE;
 	while (tokens)
 	{
-		r_type = is_redir(tokens->value);
-		if (tokens->type == PIPE)
+		token = (t_token *)tokens->content;
+		r_type = is_redir(token->value);
+		if (token->type == PIPE)
 			res = add_pipe(argvs, cmd, &last_type);
-		else if (tokens->type == REDIR)
-			res = add_redir(argvs, tokens->value, r_type, &last_type);
-		else if (tokens->type == WORD)
-			res = add_word(argvs, cmd, tokens->value, &last_type);
+		else if (token->type == REDIR)
+			res = add_redir(argvs, token->value, r_type, &last_type);
+		else if (token->type == WORD)
+			res = add_word(argvs, cmd, token->value, &last_type);
 		if (res)
 			return (FAIL);
 		tokens = tokens->next;
