87a318a740285c0cf28029a15df4100cff08193a
채우석 <wochae@wochae-MacBookAir.local>
2022-09-28 14:03:57 +0900

    [Feat] 파싱 치환 관련 #6

--- srcs/parse/parse.c
+++ srcs/parse/parse.c
@@ -12,9 +12,33 @@ static int	parse_argvs(t_argv *argvs, t_list *chunks)
 	return (SUCCESS);
 }
 
-void	parse(t_argv **argv)
+static void read_input(char **line)
 {
+	*line = readline("minishell $ ");
+	if (!line)
+		break;
+	if (*line != '\0')
+		add_history(line);
+	if (*line != '\0' && !is_whitespace(line))
+	{
+		argv = ft_list_init(); // 인자 초기화
+}
+
+void	parse(t_argv **argv, t_env *env)
+{
+	char	*line;
+	char	*input;
+	t_list	*chunks;
 	// 우채꺼
-	// 동현이꺼
-	// 지현이꺼 parse_argvs(argvs, chunks);
+	while(1)
+	{
+		// 우채님 소스
+		read_input(&input);
+		line = replace_env(input); // line을 읽어서 존재하는 환경변수를 치환한 다음, line에 변경된 값을 덮어쓴다. 그리고 이전 주소 해제
+		// 동현이꺼
+		split_line(&chunks, line);
+		// 지현이꺼 parse_argvs(argvs, chunks);
+		parse_argvs(argv, chunks);
+		break ;
+	}
 }
