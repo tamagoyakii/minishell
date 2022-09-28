49cd0bb80586b29cd4dac18c87719a7b30187230
pearpearB <smilyoung@gmail.com>
2022-09-29 05:36:48 +0900

    [Feat] 헤더 및 유틸 함수 구현 #30

--- srcs/execute/util_env.c
+++ srcs/execute/util_env.c
@@ -14,8 +14,41 @@ t_env	*get_env(char *key)
 	return (NULL);
 }
 
-void	update_env(void)
+int	is_valid_key(char *key)
 {
-	// 있던 이중배열 프리해주는 함수 추가 예정
-	g_info.env = make_2_arr_env(g_info.env_list);
+	int	i;
+	
+	i = 0;
+	if (!(ft_isalpha(key[i]) || key[i] == '_'))
+		return (FAIL);
+	i++;
+	if (!(ft_isdigit(key[i]) || ft_isalpha(key[i]) || key[i] == '_') && key[i])
+	return (SUCCESS);
+}
+
+void	print_invalid_error(char *cmd, char *input)
+{
+	char	*message;
+	char	*tmp;
+
+	tmp = ft_strjoin(cmd, " not a valid identifier");
+	if (!tmp)
+		ft_error_exit("malloc", strerror(errno), FAIL);
+	message = ft_strjoin("export: ", tmp);
+	free(tmp);
+	ft_putstr_fd(message, 2);
+}
+
+void	insert_new_env(t_env *new)
+{
+	// 정렬 찾아서 삽입
+	// 이거 우채한테 코드 있을 것 같은데...
+}
+
+void	update_2_arr_env(void)
+{
+	free_strs(g_info.env);
+	g_info.env = make_2_arr_env(g_info.env_list); // 우채 코드
+	if(!g_info.env)
+		return ; // 우채의 코드에 따라 달라질 예정!
 }
