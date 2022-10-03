76ba93ff900e3b26df89b0e33c58d3dde1ebc50b
Heo Namjun <heonamjun@Heos-MacBook-Air.local>
2022-10-03 13:38:55 +0900

    [Fix] is_valid_path 함수 2개로 분할

--- srcs/execute/path.c
+++ srcs/execute/path.c
@@ -1,19 +1,25 @@
 #include "../../includes/execute.h"
 #include <sys/stat.h>
 
-static int	is_valid_path(char *path)
+static int	is_valid_command(char *command)
 {
 	struct stat	info;
 
-	if (!ft_strchr(path, '/'))
-		return (FALSE);
-	if (stat(path, &info) == SUCCESS)
+	if (stat(command, &info) == SUCCESS)
 	{
 		if ((info.st_mode & S_IFMT) == S_IFDIR)
-			ft_error_exit(path, "is a directory", FAIL);
+			ft_error_exit(command, "is a directory", FAIL);
 		return (TRUE);
 	}
 	return (FALSE);
+}
+
+static int	is_valid_path(char *path)
+{
+	if (!ft_strchr(path, '/'))
+		return (FALSE);
+	is_valid_command(path);
+	return (TRUE);
 }
 
 static char	*make_path(char *cmd, char **paths)
@@ -25,7 +31,7 @@ static char	*make_path(char *cmd, char **paths)
 	while (paths[++i])
 	{
 		path = ft_strjoin_3(paths[i], "/", cmd);
-		if (is_valid_path(path))
+		if (is_valid_command(path))
 			return (path);
 	}
 	free(path);
